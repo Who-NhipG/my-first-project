08f7d1aa3d9da4ea4cca1c91b526eb8c33bd822c
Nhat Tan Huynh <huynhnhattanit@gmail.com>
2026-03-10 22:08:24 +1030

    ok

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const title = document.getElementById("title");
+
+btn.addEventListener("click", function() {
+    title.textContent = "You click the button!";
+});
